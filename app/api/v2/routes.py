7153e5d6d3862cde2e07e789fd018e414251c409
Too Collins <collinstoo.ct@gmail.com>
2018-10-28 21:02:26 +0300

    [Chore #161485514] Add routes for auth signup and login

--- app/api/v2/routes.py
+++ app/api/v2/routes.py
@@ -1,6 +1,10 @@
 from flask_restful import Api, Resource
 
 from . import v2_blueprint, auth_v2_blueprint
+from .views import authorization
 
 API = Api(v2_blueprint)
 API_AUTH = Api(auth_v2_blueprint)
+
+API_AUTH.add_resource(authorization.SignUp, '/signup')
+API_AUTH.add_resource(authorization.Login, '/login')
