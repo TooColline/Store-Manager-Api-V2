8079ac65847692cb75f94c6bab629cda1f4d35e3
Too Collins <collinstoo.ct@gmail.com>
2018-11-08 17:57:03 +0300

    [Chore #161577892] Add more tests to increase coverage

--- tests/v2/test_products.py
+++ tests/v2/test_products.py
@@ -37,8 +37,6 @@ class Products(base_test.BaseTestClass):
     def test_add_new_product_price_negative(self):
         """Test POST /products with the price of a negative number or zero"""
 
-        
-
         response = self.app_test_client.post('{}/products'.format(
             self.base_url), json={
                 'name': "Hand Bag", 'price': -1, 'min_quantity': 7, 'inventory': 7, 'category':'Clothing'
@@ -50,6 +48,20 @@ class Products(base_test.BaseTestClass):
         self.assertEqual(general_helper_functions.convert_json(
             response)['message'],
             'Price of the product should be a positive integer')
+
+    def test_add_new_product_missing_parameter(self):
+
+        response = self.app_test_client.post('{}/products'.format(
+            self.base_url), json={
+                'name': "Hand Bag", 'price': -1, 'inventory': 7, 'category':'Clothing'
+                }, headers=dict(Authorization=self.token),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+
+        self.assertEqual(general_helper_functions.convert_json(
+            response)['message'],
+            'Bad request. Request missing a required parameter')
 
     def test_add_new_product_name_not_string(self):
         """Test POST /products with a product name different from string"""
@@ -122,6 +134,19 @@ class Products(base_test.BaseTestClass):
         self.assertEqual(general_helper_functions.convert_json(
             response)['message'],
             'All the products have been fetched successfully')
+
+    def test_get_all_products_not_found(self):
+
+        response = self.app_test_client.get(
+            '{}/products'.format(self.base_url),
+            headers=dict(Authorization=self.token),
+            content_type='application/json'
+            )
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(general_helper_functions.convert_json(
+            response)['message'],
+            'Sorry, there are no products in the database yet')
     
     def test_get_specific_product(self):
         """Test GET /products/<int:product_id> if the product is available in store"""
@@ -149,12 +174,15 @@ class Products(base_test.BaseTestClass):
         """Test GET /products/<int:product_id> - when product does not exist"""
 
         response = self.app_test_client.get(
-            '{}/product/1000'.format(self.base_url),
+            '{}/products/1000'.format(self.base_url),
             headers=dict(Authorization=self.token),
             content_type='application/json'
             )
 
         self.assertEqual(response.status_code, 404)
+        self.assertEqual(general_helper_functions.convert_json(
+            response)['message'],
+            'Product with id 1000 is not existing')
         
     def test_modify_product(self):
         """PUT /product/"""
@@ -204,6 +232,18 @@ class Products(base_test.BaseTestClass):
             content_type='application/json')
 
         self.assertEqual(response.status_code, 404)
+    
+    def test_request_not_json_format(self):
+        """Test POST /products"""
+        
+        response = self.app_test_client.post('{}/products'.format(
+            self.base_url), json=self.Product, headers=dict(Authorization=self.token),
+            content_type='application/text')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(general_helper_functions.convert_json(
+            response)['message'], 'Bad request. Request must be in json format')
+
 
     def test_delete_product(self):
         """DELETE /product/id"""
@@ -224,3 +264,15 @@ class Products(base_test.BaseTestClass):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(general_helper_functions.convert_json(
             response)['message'], "Product has been deleted successfully")
+    
+    def test_delete_product_not_existing(self):
+        """DELETE /products/<int:product_id>"""
+
+        response = self.app_test_client.delete('{}/products/1000'.format(
+            self.base_url),
+            headers=dict(Authorization=self.token),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(general_helper_functions.convert_json(
+            response)['message'], "Product of id 1000 is not available")
