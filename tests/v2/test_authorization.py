9bb083a862ad2b361ad976c790b6c98f8f913066
Too Collins <collinstoo.ct@gmail.com>
2018-11-08 17:56:20 +0300

    [Chore #161485514] Add more tests to increase coverage

--- tests/v2/test_authorization.py
+++ tests/v2/test_authorization.py
@@ -8,6 +8,22 @@ from app.api.v2.db import initialize_db
 
 class TestUserAuthEndpoints(base_test.BaseTestClass):
 
+    def test_missing_token(self):
+        """Test GET /products - when token is missing"""
+        token = ""
+
+        response = self.app_test_client.get(
+            '{}/products'.format(self.base_url),
+            headers=dict(Authorization=token),
+            content_type='application/json'
+            )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(general_helper_functions.convert_json(
+            response)["Message"], "You need to login")
+
+    
+
     def test_add_new_user_with_all_data(self):
 
         res = self.app_test_client.post("api/v2/auth/signup",
@@ -24,6 +40,22 @@ class TestUserAuthEndpoints(base_test.BaseTestClass):
 
         self.assertEqual(data['user']['email'], "newuser@gmail.com")
         self.assertEqual(res.status_code, 200)
+    
+    def test_add_new_user_already_existing(self):
+        """Test POST /auth/signup"""
+        
+        response = self.app_test_client.post("api/v2/auth/signup",
+        json={
+        "email": "admin@gmail.com",
+        "role": "Admin",
+        "password": "Password2#"
+        }, 
+        headers=dict(Authorization=self.token),
+            content_type='application/json')
+        data = json.loads(response.data.decode())
+
+        self.assertEqual(response.status_code, 409)
+        self.assertEqual(data['message'], "User with that email already exists")
 
     def test_add_new_user_with_no_data(self):
 
@@ -187,6 +219,35 @@ class TestUserAuthEndpoints(base_test.BaseTestClass):
         self.assertTrue(general_helper_functions.convert_json(
         resp)['message'], "You are successfully logged in!")
         self.assertEqual(resp.status_code, 200)
+    
+    def test_login_user_not_existing_with_email(self):
+
+        resp = self.app_test_client.post("api/v2/auth/login",
+        json={
+            "email": "userwrongcredentials@gmail.com",
+            "password": "notevensureofpassword"
+        },
+        headers={
+        "Content-Type": "application/json"
+        })
+
+        self.assertEqual(general_helper_functions.convert_json(
+        resp)['message'], "Sorry, cannot find user with that email")
+    
+    def test_user_login_with_wrong_password(self):
+
+        response = self.app_test_client.post("api/v2/auth/login",
+        json={
+            "email": "admin@gmail.com",
+            "password": "notevensureofpassword"
+        },
+        headers={
+        "Content-Type": "application/json"
+        })
+
+        self.assertTrue(general_helper_functions.convert_json(
+        response)['message'], "Wrong credentials entered please try again")
+        self.assertEqual(response.status_code, 403)
 
     def test_logout_user(self):
 
@@ -198,4 +259,29 @@ class TestUserAuthEndpoints(base_test.BaseTestClass):
         self.assertEqual(general_helper_functions.convert_json(
             response)['message'], "Logged out successfully"
         )
+    
+    # def test_abort_user_if_not_admin(self):
+    #     token = self.login_attendant_test()
+
+    #     response = self.app_test_client.post('{}/products'.format(
+    #         self.base_url), json={'name': 'Carpet', 'price': 17000, 'min_quantity': 7, 'inventory': 7,
+    #         'category': 'Home & Furniture'}, headers=dict(Authorization=token),
+    #         content_type='application/json')
+
+    #     self.assertEqual(general_helper_functions.convert_json(
+    #     response)['message'], "Unauthorized access, please contact your administrator!")
+    #     self.assertEqual(response.status_code, 401)
+
+    def test_invalid_token_provided(self):
+
+        token = "invalid.token.provided"
+        response = self.app_test_client.get(
+            '{}/products'.format(self.base_url),
+            headers=dict(Authorization=token),
+            content_type='application/json'
+            )
+
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(general_helper_functions.convert_json(
+            response)["Message"], "This token is invalid")
     
